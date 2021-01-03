574abd7e66397017bf2e24c6f0db9474312b63db
ups3t <upset3e5@protonmail.com>
2021-01-03 15:48:26 -0500

    Initial Commit

---
+++ build/CMakeFiles/yodafier.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yodafier.dir/main.cpp.o"
+  "yodafier"
+  "yodafier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yodafier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+std::string yodafy(const std::vector<std::string>& string_vector) {
+
+   std::string retval;
+
+   if(string_vector.size() < 3) { // needs at least 3 words for yodafiable sentence (subject and predicate)
+     std::cerr << "A sentence needs at least 3 words in it." << std::endl; // Tell user to make a better sentence
+     exit(0);
+   }
+
+   for (int i = 2; i < string_vector.size(); i++) { // For loop through the every word after the first two
+      retval += string_vector[i]; // Add these words to the result first (because that is how yoda speaks)
+      retval += (i == string_vector.size() - 1 ? "" : " "); // We make sure that we don't put a space between the last word and the comma
+   }
+
+   retval += ","; // Add comma
+   retval += " " + string_vector[0] + " " + string_vector[1]; // Add first two words
+
+   return retval;
+
+}
+
+bool ends_with_period(const std::string& text) {
+   return text.at(text.size() - 1) == '.';
+}
+
+int main(int argc, char** argv) {
+
+   int start = 1;
+   int end;
+
+   for (int i = start; i < argc; i++) {
+
+      std::string patched_string = argv[i];
+      if (i == argc - 1) {
+          if(!ends_with_period(patched_string)) patched_string = patched_string + "."; // We add a period internally to the last word so that it doesn't break the following code.
+      }
+
+      if (ends_with_period(patched_string)) { // This section does a for loop of the text provided, and when it hits a period, it executes the yodafier
+
+          end = i;
+
+          std::vector<std::string> yodafied;
+
+          for (int j = start; j <= end; j++) {
+             std::string word = std::string(argv[j]);
+             std::string result = (j == end && ends_with_period(word) ? std::string(word).substr(0, word.size() - 1) : word);
+             yodafied.emplace_back(result);
+          }
+
+          std::cout << yodafy(yodafied) << std::endl;
+
+          
+          start = (end == argc - 1 ? end : end + 1); 
+      }
+   }
+
+   return 0;
+}
+
